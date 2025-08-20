2d0f98b9b54d711450ffa379515967368e856ea5
Upaat <miiiupaat@gmail.com>
2025-08-20 10:46:40 +0800

    intalled next15 and tailwindv3 and initialize Header UI

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,14 @@
+const page = () => {
+  return (
+    <>
+      <h1 className="text-3xl h-100 text-red-500 font-bold underline">Home</h1>
+      <h1 className="text-3xl h-100 text-red-500 font-bold underline">Home</h1>
+      <h1 className="text-3xl h-100 text-red-500 font-bold underline">Home</h1>
+      <h1 className="text-3xl h-100 text-red-500 font-bold underline">Home</h1>
+      <h1 className="text-3xl h-100 text-red-500 font-bold underline">Home</h1>
+      <h1 className="text-3xl h-100 text-red-500 font-bold underline">Home</h1>
+    </>
+  );
+};
+
+export default page;
---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Ebaby";
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || "Grow more, glow more";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "An Ebay clone built with Next.js and MongoDB";
