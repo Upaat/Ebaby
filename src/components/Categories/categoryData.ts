1ff69e73484fd9402614c23f1d214012b6d78cb1
frience0 <marckfrience0@gmail.com>
2025-08-21 08:37:26 +0800

    category functionality

---
+++ src/components/Categories/categoryData.ts
@@ -0,0 +1,76 @@
+const data = [
+  {
+    title: "Oversized",
+    id: 1,
+    img: "/images/categories/oversized.png",
+    link: {
+      text: "View All",
+      href: "/search?tag=new-arrival",
+    },
+  },
+  {
+    title: "Tshirts",
+    id: 2,
+    img: "/images/categories/tshirt.png",
+    link: {
+      text: "View All",
+      href: "/search?tag=new-arrival",
+    },
+  },
+  {
+    title: "Women",
+    id: 3,
+    img: "/images/categories/women.png",
+    link: {
+      text: "View All",
+      href: "/search?tag=new-arrival",
+    },
+  },
+  {
+    title: "Hoodie",
+    id: 4,
+    img: "/images/categories/hoodie.png",
+    link: {
+      text: "View All",
+      href: "/search?tag=new-arrival",
+    },
+  },
+  {
+    title: "Kids",
+    id: 5,
+    img: "/images/categories/kids.png",
+    link: {
+      text: "View All",
+      href: "/search?tag=new-arrival",
+    },
+  },
+  {
+    title: "Cute",
+    id: 6,
+    img: "/images/categories/cute.png",
+    link: {
+      text: "View All",
+      href: "/search?tag=new-arrival",
+    },
+  },
+  {
+    title: "Food",
+    id: 7,
+    img: "/images/categories/food.png",
+    link: {
+      text: "View All",
+      href: "/search?tag=new-arrival",
+    },
+  },
+  {
+    title: "Space",
+    id: 8,
+    img: "/images/categories/space.png",
+    link: {
+      text: "View All",
+      href: "/search?tag=new-arrival",
+    },
+  },
+];
+
+export default data;
---
+++ src/types/category.ts
@@ -0,0 +1,19 @@
+export type Category = {
+  title: string;
+  id: number;
+  img: string;
+  link?: { text: string; href: string };
+};
+
+// export type Category = {
+//   title: string;
+//   id: number;
+//   link: { text: string; href: string };
+//   items: {
+//     name: string;
+//     items?: string[];
+//     img: string;
+//     href: string;
+//   }[];
+//   // img: string;
+// };
